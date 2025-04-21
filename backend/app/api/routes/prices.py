d1731304f51e6cb770819bc7ddb57fa5aa556f3d
Ivarsky <iamh90@pm.me>
2025-04-21 12:19:51 +0000

    pagination, delete station_id from response and adding addres

--- backend/app/api/routes/prices.py
+++ backend/app/api/routes/prices.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request, Response
 from app.models.station_fuel_price import StationFuelPrice
 from app.models.fuel import Fuel
 from app.models.station import Station
 from app import db
+import json
 
 prices_bp = Blueprint("prices", __name__)
 
@@ -10,7 +11,8 @@ prices_bp = Blueprint("prices", __name__)
 @prices_bp.route("ranking", methods=["GET"])
 def get_price_ranking():
     fuel_params = request.args.getlist("fuel")
-    limit = int(request.args.get("limit", 100))
+    limit = int(request.args.get("limit", 10))
+    offset = int(request.args.get("offset", 0))
 
     query = (
         db.session.query(StationFuelPrice)
@@ -23,15 +25,17 @@ def get_price_ranking():
         fuel_params = [f.upper() for f in fuel_params]
         query = query.filter(Fuel.id.in_(fuel_params))
 
-    results = query.order_by(StationFuelPrice.price.asc()).limit(limit).all()
+    results = (
+        query.order_by(StationFuelPrice.price.asc()).offset(offset).limit(limit).all()
+    )
 
     response = []
     for p in results:
         response.append(
             {
-                "station_id": p.id_station,
-                "station_name": p.station.name,
                 "brand": p.station.brand.name,
+                "station_name": p.station.name,
+                "address": p.station.address,
                 "fuel_type": p.fuel.name,
                 "price": float(p.price),
                 "discount": float(p.discount) if p.discount is not None else None,
@@ -39,4 +43,6 @@ def get_price_ranking():
             }
         )
 
-    return jsonify(response)
+    return Response(
+        json.dumps(response, ensure_ascii=False), content_type="application/json"
+    )

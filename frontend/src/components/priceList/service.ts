e720b932a1dd6ccb1556c74c42f81bd0ed1fac22
Ivarsky <iamh90@pm.me>
2025-05-24 12:48:56 +0000

    reformat: service.ts url string

--- frontend/src/components/priceList/service.ts
+++ frontend/src/components/priceList/service.ts
@@ -1,8 +1,7 @@
 import client from '../../api/client.ts';
 
 export const getLatestPrices = (region?: string) => {
-  const limit = '100';
-  const baseUrl = `/prices?fuel=GAS&fuel=DIESEL&limit=${limit}`;
-  const url = region ? `/prices?region=${region}&fuel=GAS&fuel=DIESEL` : baseUrl;
+  const baseUrl = `/prices?fuel=GAS&fuel=DIESEL`;
+  const url = region ? `${baseUrl}&region=${region}` : baseUrl;
   return client.get(url);
 };
